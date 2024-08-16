85854362e9e76cc9ca84563976fada22757dc4d2
Jwalker830 <jw.r830@gmail.com>
2024-08-16 15:53:23 -0700

    adding port?

--- main.py
+++ main.py
@@ -35,4 +35,5 @@ def remove_background():
     return jsonify({'success': False, 'error': 'Error processing file'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
